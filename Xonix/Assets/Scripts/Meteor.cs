cddb57d7c1c6b37db5858f7576d8b80bab60b7fd
ejoseph <ejoseph@magicleap.com>
2018-05-19 23:24:07 +0300

    General update

---
+++ Xonix/Assets/Scripts/Meteor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour {
+
+	public float speed = 30f;
+	public float init_torque = 0.02f;
+	private Rigidbody2D rb;
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+
+		// Generate random angle for initial force vector
+		float ang = Random.value * 2 * Mathf.PI;
+		Vector2 force_vec = new Vector2(Mathf.Cos(ang), Mathf.Sin(ang));
+		rb.AddForce(force_vec*speed);
+		rb.AddTorque(init_torque);
+		Debug.Log("Set position: " + this.transform.position.ToString());
+	}
+	
+	// void OnCollisionEnter2D(Collision2D collision){
+	// 	Debug.Log("Collision detected");
+	// }
+
+}
---
+++ Xonix/Assets/Scripts/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		// rb.AddForce(movement * speed);
+		Vector3 newPos = this.transform.position + movement * speed * Time.deltaTime;
+		newPos.x = Mathf.Clamp(newPos.x, -6.4f, 6.4f);
+		newPos.y = Mathf.Clamp(newPos.y, -4.75f, 3.7f);
+		this.transform.position = newPos;
+    }
+
+	// void OnCollisionEnter2D(Collision2D collision){
+	// 	Debug.Log("Ship collided");
+	// }
+
+	// void OnTriggerEnter2D(Collider collider){
+	// 	Debug.Log("Ship triggered");
+	// }
+}
